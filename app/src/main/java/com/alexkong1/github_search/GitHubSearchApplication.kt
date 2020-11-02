1e73dad0c7d0cd58c5ace8eb5aaa702a3e2ef157
alexkong1 <akong@gwmail.gwu.edu>
2020-11-01 19:16:29 -0800

    add search box and construct API call

--- app/src/main/java/com/alexkong1/github_search/GitHubSearchApplication.kt
+++ app/src/main/java/com/alexkong1/github_search/GitHubSearchApplication.kt
@@ -1,10 +1,23 @@
 package com.alexkong1.github_search
 
 import android.app.Application
+import com.alexkong1.github_search.data.GithubApi
+import retrofit2.Retrofit
 
 class GitHubSearchApplication: Application() {
 
+    lateinit var service: GithubApi
+
     override fun onCreate() {
         super.onCreate()
+        initService()
+    }
+
+    private fun initService() {
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://api.github.com")
+            .build()
+
+        service = retrofit.create(GithubApi::class.java)
     }
 }
--- app/src/main/java/com/alexkong1/github_search/ui/MainActivity.kt
+++ app/src/main/java/com/alexkong1/github_search/ui/MainActivity.kt
@@ -2,11 +2,32 @@ package com.alexkong1.github_search.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
 import com.alexkong1.github_search.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initUi()
+    }
+
+    private fun initUi() {
+        val searchBox = findViewById<EditText>(R.id.et_search_box)
+        searchBox.addTextChangedListener( object : TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                TODO("Not yet implemented")
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                TODO("Not yet implemented")
+            }
+        })
     }
 }
