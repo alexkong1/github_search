86a575225fb0e62a70ceae459667daa5ccfbc031
alexkong1 <akong@gwmail.gwu.edu>
2020-11-02 20:39:30 -0800

    update to display user names from search results

--- app/src/main/java/com/alexkong1/github_search/ui/SearchResultsAdapter.kt
+++ app/src/main/java/com/alexkong1/github_search/ui/SearchResultsAdapter.kt
@@ -5,12 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import androidx.core.content.getSystemService
 import androidx.recyclerview.widget.RecyclerView
 import com.alexkong1.github_search.R
-import com.alexkong1.github_search.data.model.Item
+import com.alexkong1.github_search.data.model.User
 
-class SearchResultsAdapter(private val context: Context?, private val results: List<Item>): RecyclerView.Adapter<SearchResultsAdapter.ViewHolder>() {
+class SearchResultsAdapter(private val context: Context?, private var users: List<User>): RecyclerView.Adapter<SearchResultsAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val inflater = context?.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
@@ -18,16 +17,21 @@ class SearchResultsAdapter(private val context: Context?, private val results: L
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.onBind(results[position])
+        holder.onBind(users[position])
     }
 
     override fun getItemCount(): Int {
-        return results.size
+        return users.size
+    }
+
+    fun updateUsers(results: List<User>) {
+        users = results
+        notifyDataSetChanged()
     }
 
     class ViewHolder constructor(view: View): RecyclerView.ViewHolder(view) {
-        fun onBind(item: Item) {
-            itemView.findViewById<TextView>(R.id.tv_search_result_username).text = item.name
+        fun onBind(user: User) {
+            itemView.findViewById<TextView>(R.id.tv_search_result_username).text = user.login
         }
     }
 }
--- app/src/main/java/com/alexkong1/github_search/ui/SearchResultsFragment.kt
+++ app/src/main/java/com/alexkong1/github_search/ui/SearchResultsFragment.kt
@@ -1,14 +1,25 @@
 package com.alexkong1.github_search.ui
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.alexkong1.github_search.GitHubSearchApplication
 import com.alexkong1.github_search.R
 import com.alexkong1.github_search.data.model.Item
+import com.alexkong1.github_search.data.model.SearchUserResult
+import com.alexkong1.github_search.data.model.User
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import java.lang.Exception
 
 class SearchResultsFragment: Fragment() {
 
@@ -38,8 +49,42 @@ class SearchResultsFragment: Fragment() {
     }
 
     private fun initUi(root: View) {
+
         recyclerView = root.findViewById(R.id.rv_search_results)
         recyclerView.layoutManager = LinearLayoutManager(context)
-        recyclerView.adapter = SearchResultsAdapter(context, listOf<Item>())
+
+        val searchBox = root.findViewById<EditText>(R.id.et_search_box)
+        searchBox.addTextChangedListener( object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                //TODO("Not yet implemented")
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                GlobalScope.launch(Dispatchers.Main) {
+                    try {
+                        val response =
+                            (context?.applicationContext as GitHubSearchApplication).getRetrofit()
+                                .searchUser(s.toString())
+                        Log.e("API CALL", response.toString())
+
+                        updateRecycler(response.items)
+                    } catch (e: Exception) {
+                        Log.e("API CALL", "failed $e")
+                    }
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                //TODO("Not yet implemented")
+            }
+        })
+    }
+
+    private fun updateRecycler(results: List<User>) {
+        if (recyclerView.adapter != null) {
+            (recyclerView.adapter as SearchResultsAdapter).updateUsers(results)
+        } else {
+            recyclerView.adapter = SearchResultsAdapter(context, results)
+        }
     }
 }
